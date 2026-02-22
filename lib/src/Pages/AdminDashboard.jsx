de3e44e36571c7cfb4613a1e2b81f07e8a2c419d
Risingdell <dhanush718718m@gmail.com>
2026-02-22 17:52:20 +0530

    ver-mer-work2

--- lib/src/Pages/AdminDashboard.jsx
+++ lib/src/Pages/AdminDashboard.jsx
@@ -395,7 +395,7 @@ const AdminDashboard = () => {
   if (!admin) {
     // This should never be reached as we redirect in useEffect
     //Dhanush M-2026-22-2
-    //git check
+    //git check1
     return null;
   }
 
